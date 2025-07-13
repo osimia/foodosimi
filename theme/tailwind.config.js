45f01eecf3f49d6802eb7c61244d79dc20481b22
Akramkhon Osimi <6721067659@my.lerna.tj>
2025-07-13 11:39:16 +0500

    Обновление названия сайта на OsimiFood с оранжевым цветом логотипа

--- theme/tailwind.config.js
+++ theme/tailwind.config.js
@@ -11,6 +11,12 @@ module.exports = {
           dark: '#0041C4',
           light: '#F6F6F6',
         },
+        osimi: {
+          DEFAULT: '#FF6B35',
+          dark: '#E85A2B', 
+          light: '#FF8A5B',
+          bright: '#FF4500',
+        },
       },
       fontFamily: {
         sans: ['"Nunito Sans"', 'sans-serif'],
